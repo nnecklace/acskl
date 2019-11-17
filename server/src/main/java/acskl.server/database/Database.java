df7268041cc9ec38880c14ffdbf248138d9a6f01
Niclas Forsman <niclas.forsman@futurice.com>
2019-11-17 17:48:08 +0200

    update database layer

--- server/src/main/java/acskl.server/database/Database.java
+++ server/src/main/java/acskl.server/database/Database.java
@@ -1,14 +1,19 @@
 package acskl.server.database; 
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
+import java.sql.Statement;
 import java.util.Arrays;
 
 public class Database {
     private String CONNECTION_STRING = "jdbc:sqlite:database.db";
     private Connection db;
     private PreparedStatement statement;
+    private ResultSet result;
 
     public Database establish() {
         try {
@@ -20,30 +25,57 @@ public class Database {
         return this;
     }
 
-    public Database query(String query) throws SQLException {
-        if (db != null) {
-            statement = db.prepareStatement(query);
+    public Database query(String query) {
+        try {
+            if (db != null) statement = db.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+        } catch (SQLException exception) {
+            System.err.println("Could not create query: " + query + " " + exception.getMessage());
         }
 
         return this;
     }
 
-    public Database addValue(String value) throws SQLException {
-        if (statement != null) {
-            statement.setString(1, value);
+    public Database addValue(String value) {
+        try {
+            if (statement != null) statement.setString(1, value);
+        } catch(SQLException exception) {
+            System.err.println("Could not add value " + value + " " + exception.getCause());
         }
 
         return this;
     }
 
-    public void execute() throws SQLException {
-        if (statement != null) {
-            statement.execute();
+    public boolean execute() {
+        try {
+            if (statement != null) {
+                statement.execute();
+                ResultSet result = statement.getGeneratedKeys();
+
+                if (result == null) return false;
+
+                return result.next();
+            }
+        } catch (SQLException exception) {
+            System.err.println("Could not perform query: " + exception.getMessage());
         }
+  
+        return false;
+    }
+
+    public Database executeReturning() {
+        try {
+            if (statement != null) result = statement.executeQuery();
+        } catch (SQLException exception) {
+            System.err.println("Could not perform query: " + exception.getMessage());
+        }
+
+        return this;
     }
 
     public void close() {
         try {
+            if (result != null) result.close();
+            if (statement != null) statement.close();
             if (db != null) db.close();
         } catch (SQLException exception) {
             System.err.println("Could not close connection to database " + exception.getMessage());
@@ -52,6 +84,10 @@ public class Database {
 
     public PreparedStatement getStatement() {
         return statement;
+    }
+
+    public ResultSet getResult() {
+        return result;
     }
 
     public Connection getDb() {
--- server/src/test/java/acskl.server/database/DatabaseTest.java
+++ server/src/test/java/acskl.server/database/DatabaseTest.java
@@ -1,9 +1,9 @@
 package acskl.server.database;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import org.junit.Test;
@@ -11,6 +11,7 @@ import org.junit.After;
 import org.junit.Before;
 
 import acskl.server.database.Database;
+import acskl.server.services.UserService;
 
 public class DatabaseTest {
     private Database db;
@@ -28,6 +29,7 @@ public class DatabaseTest {
     public void testsSuccessfulDbConnection() {
         Database k = db.establish();
         assertTrue(k.getDb() != null);
+
     }
 
     @Test
@@ -41,5 +43,38 @@ public class DatabaseTest {
     public void testQueryWithUnSuccessfulConnection() throws SQLException {
         db.query("SELECT * FROM users");
         assertTrue(db.getStatement() == null);
+    }
+
+    @Test
+    public void testQueryExecution() throws SQLException {
+        // Note! JUnit executes tests in arbitrary order
+        // ideally tests cases should be separeted but we need to execute these in precise order
+        Database k = db.establish();
+
+        boolean a = k.query("INSERT INTO users (name) VALUES (?)")
+                        .addValue(UserService.RESERVED_USERNAME)
+                        .execute();
+
+        assertTrue("User was not created", a);
+
+        ResultSet r = k.query("SELECT * FROM users WHERE name = ?")
+                            .addValue(UserService.RESERVED_USERNAME)
+                            .executeReturning()
+                            .getResult();
+
+        if (r == null) {
+            fail("User could not be retrieved");
+        } else {
+            r.next();
+            String username = r.getString(2);
+            assertTrue("Test user was not successfully created or could not be found" , UserService.RESERVED_USERNAME.equals(username));
+        }
+
+        boolean b = k.query("DELETE FROM users WHERE name = ?")
+                        .addValue(UserService.RESERVED_USERNAME)
+                        .execute();
+
+        assertTrue("User not deleted", b);
+
     }
 }
