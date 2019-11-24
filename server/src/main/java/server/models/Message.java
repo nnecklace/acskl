f88239e02858625dcb6b8845c51a967111f237c6
Niclas Forsman <niclas.forsman@futurice.com>
2019-11-24 22:19:20 +0200

    add Message model

---
+++ server/src/main/java/server/models/Message.java
@@ -0,0 +1,37 @@
+package server.models;
+
+public class Message implements Model {
+    private int id;
+    private String content;
+    // Unix timestamp
+    private long timestamp;
+    private int userId;
+
+    public Message(){}
+
+    public Message(
+        String content,
+        long timestamp,
+        int userId
+    ) {
+        this.content = content;
+        this.timestamp = timestamp;
+        this.userId = userId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+}
