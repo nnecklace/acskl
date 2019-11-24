2d83a9cbdeda2339a1732145926a6d917da51f63
Niclas Forsman <niclas.forsman@futurice.com>
2019-11-24 22:23:17 +0200

    add basic TCP listener

--- server/src/main/java/server/App.java
+++ server/src/main/java/server/App.java
@@ -1,7 +1,29 @@
 package server;
 
-public class App {
-    public static void main(String[] args) {
+import java.net.ServerSocket;
+import java.net.Socket;
 
+import java.io.*;
+
+public class App {
+    public static void main(String[] args) throws IOException {
+        ServerSocket welcomeSocket = new ServerSocket(6789);
+        while (true) {
+            System.out.println("Server listening on port 6789");
+            System.out.println("Waiting for connections");
+            Socket connectionSocket = welcomeSocket.accept();
+            System.out.println("Someone connected!");
+            while (true) {
+                System.out.println("Waiting for messages...");
+                BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+                String clientSentence = inFromClient.readLine();
+                if (clientSentence == null) {
+                    System.out.println(welcomeSocket.getInetAddress() + " disconnected");
+                    connectionSocket.close();
+                    break;
+                }
+                System.out.println("Received: " + clientSentence);
+            }
+        }
     }
 }
