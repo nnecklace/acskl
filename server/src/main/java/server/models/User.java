446cbab2ee228aecfc6fa36aba0db593473cc917
Niclas Forsman <niclas.forsman@futurice.com>
2019-11-24 22:18:51 +0200

    implement model on User

--- server/src/main/java/server/models/User.java
+++ server/src/main/java/server/models/User.java
@@ -1,7 +1,12 @@
 package server.models;
 
-public class User {
+public class User implements Model {
     private String name;
+    // Id will automatically be mapped to this property by jackson. No need for setter
+    private int id;
+
+    // This is for jackson databind ObjectMapper
+    public User() {}
 
     public User (String name) {
         this.name = name;
@@ -9,5 +14,9 @@ public class User {
 
     public String getName() {
         return name;
+    }
+
+    public int getId() {
+        return id;
     }
 }
