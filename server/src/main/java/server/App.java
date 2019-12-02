2530d57b91981df2d9947d890cb85eebb0d346b4
Niclas Forsman <niclas.forsman@futurice.com>
2019-12-02 20:17:15 +0200

    parse incoming commands with interceptor

--- server/src/main/java/server/App.java
+++ server/src/main/java/server/App.java
@@ -3,11 +3,19 @@ package server;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+import server.services.UserService;
+import server.database.Database;
+import server.utils.Interceptor;
+
 import java.io.*;
 
 public class App {
     public static void main(String[] args) throws IOException {
         ServerSocket welcomeSocket = new ServerSocket(6789);
+        Database db = new Database();
+        UserService userService = new UserService(db);
+        Interceptor interceptor = new Interceptor(userService);
+
         while (true) {
             System.out.println("Server listening on port 6789");
             System.out.println("Waiting for connections");
@@ -16,13 +24,18 @@ public class App {
             while (true) {
                 System.out.println("Waiting for messages...");
                 BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-                String clientSentence = inFromClient.readLine();
-                if (clientSentence == null) {
+                String input = inFromClient.readLine();
+
+                String response = interceptor.parse(input);
+
+                DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+                outToClient.writeBytes(response + "\n");
+
+                if (input == null) {
                     System.out.println(welcomeSocket.getInetAddress() + " disconnected");
                     connectionSocket.close();
                     break;
                 }
-                System.out.println("Received: " + clientSentence);
             }
         }
     }
