9a4a84feab141678460fc5824ee6a92f36d94dc0
Niclas Forsman <niclas.forsman@futurice.com>
2019-12-13 16:56:38 +0200

    implement concurrent connections to server

--- server/src/main/java/server/App.java
+++ server/src/main/java/server/App.java
@@ -1,42 +1,28 @@
 package server;
 
-import java.net.ServerSocket;
-import java.net.Socket;
-
 import server.services.MessageService;
 import server.services.UserService;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
 import server.database.Database;
 import server.utils.Interceptor;
 
-import java.io.*;
-
 public class App {
-    public static void main(String[] args) throws IOException {
-        ServerSocket welcomeSocket = new ServerSocket(6789);
+    public static void main(String[] args) {
+        // setup
         Database database = new Database();
         UserService userService = new UserService(database);
         MessageService messageService = new MessageService(database);
         Interceptor interceptor = new Interceptor(userService, messageService);
-
-        while (true) {
-            System.out.println("Server listening on port 6789");
-            Socket connectionSocket = welcomeSocket.accept();
-            System.out.println("Someone connected");
-            while (true) {
-                BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-                System.out.println("Waiting for message");
-                String input = inFromClient.readLine();
-                System.out.println(input + " recevied");
-
-                if (input == null) {
-                    connectionSocket.close();
-                    break;
-                }
-
-                String response = interceptor.parse(input);
-                DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
-                outToClient.writeBytes(response + "\n");
-            }
+        try {
+            Server server = new Server(new ServerSocket(6789), interceptor);
+            // start server
+            server.launch();
+        } catch (IOException e) {
+            System.err.println("Could not start server: " + e.getMessage());
+            System.exit(1);
         }
     }
 }
---
+++ server/src/test/java/server/ConnectionTest.java
@@ -0,0 +1,231 @@
+package server;
+
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutput;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.io.Reader;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import server.database.Database;
+import server.models.Message;
+import server.models.User;
+import server.services.MessageService;
+import server.services.UserService;
+import server.utils.Interceptor;
+
+public class ConnectionTest {
+    private Connection connection;
+    private final PrintStream originalErr = System.err;
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    private BufferedReader mockedReader;
+
+    @Before
+    public void setUp() throws IOException {
+        System.setErr(new PrintStream(errContent));
+        Socket s = new Socket() {
+            public InputStream getInputStream() {
+                return new InputStream(){
+                    @Override
+                    public int read() throws IOException {
+                        return 1;
+                    }
+                };
+            }
+            public OutputStream getOutputStream() {
+                return new OutputStream(){
+                    @Override
+                    public void write(int b) throws IOException {
+                        
+                    }
+                };
+            }
+        };
+        Database database = new Database() {};
+        UserService userServiceFake = new UserService(database) {
+            List<String> usernames;
+            public UserService start() {
+                usernames = new ArrayList<>();
+                usernames.add("William");
+                return this;
+            }
+            public boolean login(String username) {
+                if (usernames.contains(username)) {
+                    return true;
+                }
+                return false;
+            }
+            public User create(String username) {
+                if (!usernames.contains(username)) {
+                    return new User(username);
+                }
+                return null;
+            }
+        }.start();
+
+        MessageService messageServiceFake = new MessageService(database) {
+            List<Message> messages;
+            public Message create(String content, int timestamp, int userId) {
+                if (userId == 0) return null;
+                return new Message(content, timestamp, userId);
+            }
+            public List<Message> getAll() {
+                messages = new ArrayList<>();
+                messages.add(new Message("ayy lmao", 5555, 1));
+                messages.add(new Message("lol", 123, 9));
+                return messages;
+            }
+        };
+
+        Interceptor i = new Interceptor(userServiceFake,messageServiceFake);
+        this.connection = new Connection(s,i);
+
+        Reader r = new Reader(){
+            @Override
+            public int read(char[] cbuf, int off, int len) throws IOException {
+                // TODO Auto-generated method stub
+                return 0;
+            }
+            @Override
+            public void close() throws IOException {
+                // TODO Auto-generated method stub
+                
+            }
+        };
+
+        BufferedReader reader = new BufferedReader(r) {
+            int counter;
+            public String readLine() {
+                if (counter > 0) return null;
+                counter++;
+                return "USER:LOGIN:William";
+            }
+        };
+
+        DataOutput writer = new DataOutput() {
+            public String output;
+            public void writeBytes(String bytes) {
+                this.output = bytes;
+            }
+
+            @Override
+            public void write(int b) throws IOException {
+
+            }
+
+            @Override
+            public void write(byte[] b) throws IOException {
+
+            }
+
+            @Override
+            public void write(byte[] b, int off, int len) throws IOException {
+
+            }
+
+            @Override
+            public void writeBoolean(boolean v) throws IOException {
+
+            }
+
+            @Override
+            public void writeByte(int v) throws IOException {
+
+            }
+
+            @Override
+            public void writeShort(int v) throws IOException {
+
+            }
+
+            @Override
+            public void writeChar(int v) throws IOException {
+
+            }
+
+            @Override
+            public void writeInt(int v) throws IOException {
+
+            }
+
+            @Override
+            public void writeLong(long v) throws IOException {
+
+            }
+
+            @Override
+            public void writeFloat(float v) throws IOException {
+
+            }
+
+            @Override
+            public void writeDouble(double v) throws IOException {
+
+            }
+
+            @Override
+            public void writeChars(String s) throws IOException {
+
+            }
+
+            @Override
+            public void writeUTF(String s) throws IOException {
+
+            }
+
+            @Override
+            public String toString() {
+                return output;
+            }
+        };
+
+        this.connection.setReader(reader);
+        this.connection.setWriter(writer);
+    }
+
+    @After
+    public void restoreStreams() {
+        System.setErr(originalErr);
+    }
+
+    @Test
+    public void testRunWithBasicMessage() {
+        String expected = "S:USER:LOGIN:William";
+        this.connection.run();
+        String actual = this.connection.getWriter().toString();
+        assertTrue("Expected that " + expected + " would be returned but got instead " + actual, !expected.equals(actual));
+    }
+
+    @Test
+    public void testRunExitsOnNull() {
+        this.connection.run();
+        assertTrue("Expected that connection would be closed on null message", this.connection.isClosed());
+    }
+
+    @Test
+    public void testIOExceptionToBeThrownWillReading() throws IOException {
+        this.connection.run();
+
+        String expected = "Could not get input stream from client: Error";
+        String actual = errContent.toString();
+
+        assertTrue("Expected error to be " + expected + "  but was " + actual, expected.equals(actual));        
+    }
+}
